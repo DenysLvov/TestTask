537afa8dd97961385b1aae66e13bd25443d786bf
Denys <lionpdv@gmail.com>
2021-02-06 18:11:06 +0200

    Add s2, s3 s4

--- QAA_task/src/tests/StashTestClass.java
+++ QAA_task/src/tests/StashTestClass.java
@@ -3,4 +3,8 @@ package tests;
 public class StashTestClass {
 
     String s1 = "Changes made on Local PC";
+
+    String s2 = "Before stash changes";
+    String s3 = "Before stash changes";
+    String s4 = "Before stash changes";
 }
