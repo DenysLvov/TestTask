5f2445bbcbed9fb65a85c280c21ddb3dc5c38cf9
Denys <lionpdv@gmail.com>
2021-02-06 18:20:34 +0200

    Merge branch 'US_Test_Stash' of https://github.com/DenysLvov/TestTask into US_Test_Stash
    # Conflicts:
    #	QAA_task/src/tests/StashTestClass.java

--- QAA_task/src/tests/StashTestClass.java
+++ QAA_task/src/tests/StashTestClass.java
@@ -3,5 +3,15 @@ package tests;
 public class StashTestClass {
 
     String s1 = "Changes made on Local PC";
+
     String ss_10 = "Changes made on GIT";    
+
+
+    String s2 = "Before stash changes";
+    String s3 = "Before stash changes";
+    String s4 = "Before stash changes";
+
+    String s5 = "Not commited var1";
+    String s5 = "Not commited var2";
+
 }
